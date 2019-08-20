f2e025d9c056db53f462ca1d2cfc58793cc0690d
Guilherme Chinaglia <quinalia.guilherme@gmail.com>
2019-08-20 05:23:43 -0300

    Ajax com JQuery

---
+++ lista.php
@@ -0,0 +1,26 @@
+<?php
+
+
+// foreach ($_GET as $key => $value) { // $_GET
+//     echo $value;
+// }
+
+$usuarios = [
+    
+    "Guilherme",
+    "Paola",
+    "Gabriela",
+    "Marcio",
+    "Odália"
+];
+
+
+echo json_encode($usuarios);
+
+
+
+//SHORTHAND = EXEMPLO DE ENVIO DE DADOS VIA GET COM JQUERY
+
+// foreach ($_GET as $key => $value) { // $_GET
+//     echo $value;
+// }
